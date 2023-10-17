e948482ff3755156c732ebd83f84b63d92c7f515
Ismaila SY <baboyesy10@gmail.com>
2023-10-17 12:56:38 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => res.send('Hello MERN!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
